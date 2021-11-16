d1b4fd619131e829a9d3bc7498ef0401b955e700
monaquimbamba <claudio.antonio@etu.unilim.fr>
2021-11-16 10:26:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ la Synthe de texteures
---
+++ permuteur.cpp
@@ -0,0 +1,25 @@
+#include "permuteur.h"
+
+
+
+/*!
+ * Construit un nouveau permuteur qui retournera des nombres entiers dans
+ * l'intervalle [0 .. (max-1)].
+ */
+Permuteur::Permuteur(int max){
+
+}
+/**
+* Retourne l'entier suivant
+**/
+  int Permuteur::suivant()
+  {
+    return 0;
+
+  }
+
+  Permuteur::~Permuteur(){
+    /*!
+     * Libere les ressources utilisees par ce permuteur.
+     */
+  }
